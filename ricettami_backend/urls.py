9db53c00e45597beb9e02bba238f9ddab37d3cf4
Emmanuel Baah <baahemmanuel@hotmail.it>
2025-02-16 13:37:50 +0100

    Changes in the paths 2

--- ricettami_backend/urls.py
+++ ricettami_backend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api', views.myapp),
+    path('api/', views.myapp),
     path('api/login', views.login_user, name="login"),
     path('api/logout', views.logout_user, name="logout"),
     path('api/register', views.register_user, name= "register"),
